584dada35629063b55121e480115ad68216fbcbb
cameronbourgeois <cameronmbourgeois@gmail.com>
2023-07-19 14:38:49 +1200

    Add additional spec

--- spec/unleashed_spec.rb
+++ spec/unleashed_spec.rb
@@ -2,4 +2,8 @@ RSpec.describe Unleashed do
   it "has a version number" do
     expect(Unleashed::Version).not_to be nil
   end
+
+  it 'initialises client' do
+    expect{Unleashed::Client.new}.not_to raise_error
+  end
 end
